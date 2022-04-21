c6fceeaf2234d7c4a409bc807bda5e1449a5047b
vaishnaray1 <vaishnavi.narayanan@chicagobooth.edu>
2022-04-21 15:22:54 +0000

    added security based on grades criteria

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
-
+  before_action :ensure_user_is_current_user, only: %i[liked feed followers following discover ]
   private
 
     def set_user
@@ -10,4 +10,12 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
+
+    def ensure_user_is_current_user
+      if @user != current_user
+        redirect_back fallback_location: root_path, alert: "Nice try loser!"
+     end
+    end 
+
+
 end
